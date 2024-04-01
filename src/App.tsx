7c4e349ed04d2839db8fb7c96f5b228e1709f2a3
zachtsch <zachary.tschetter@usd.edu>
2024-04-01 11:36:15 -0500

    yo

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App main">
+        <h1 style={{textAlign:'center'}}>Tic-Tac-Toe</h1>
+        <h2 style={{textAlign:'center'}}>X O</h2>
+        <div className='box' style={{border:'1px solid black'}}>
+            <Rect disp='X' />
+            <Rect disp='O' />
+            <Rect disp='_' />
+        </div>
     </div>
   );
+}
+
+function Rect({disp} : {disp : 'X' | 'O' | '_'}){
+    return (<div className='rect'>
+    {disp}
+    </div>)
 }
 
 export default App;
